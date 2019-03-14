ff3fad9ee24c80423e12bc0f0df2f7bc2830f483
LotusMel <46916651+LotusMel@users.noreply.github.com>
2019-03-14 03:13:54 -0500

    Full deck instead of 20 cards

--- DeckOfCards/Deck.cs
+++ DeckOfCards/Deck.cs
@@ -4,12 +4,35 @@ namespace DeckOfCards
 {
     public class Deck
     {
-        private Random _random;
+        private Random random;
+        private string[] suits = { "Hearts", "Spades", "Clubs", "Diamonds" };
+        private string[] faceValues = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+
 
         public Deck()
         {
-            _random = new Random();
+            random = new Random();
 
+            Cards = new Card[52];
+            for (int i = 0; i < Cards.Length; i++)
+            {
+                Cards[i] = new Card();
+            }
+
+            int cardCount = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                //assign Suit
+                for (int j = 0; j < 13; j++)
+                {
+                    Card card = Cards[cardCount];
+                    card.Suit = suits[i];
+                    card.FaceValue = faceValues[j];
+                    cardCount++;
+
+                }
+
+            }
             // TODO: Load the card deck with a standard deck of 52 cards.  Use loops, do not hard code each one.
         }
 
@@ -18,7 +41,9 @@ namespace DeckOfCards
         public Card Draw()
         {
             // TODO: Return a random card from the deck.
-            return null;
+            int randomIndex = random.Next(0, 52);
+            Card card = Cards[randomIndex];
+            return card;
         }
     }
 }
